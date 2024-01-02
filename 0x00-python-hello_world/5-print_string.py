db6b69207717cac3a513492f52d39b379b8aeeb3
aragakerubo <aragakerubo@gmail.com>
2024-01-02 18:20:33 -0500

    5. Print string

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(f"Welcome to {str}!")
+# print 3 times a string stored in the variable str, followed by its first 9 characters.
+print(str * 3)
+print(str[:9])
