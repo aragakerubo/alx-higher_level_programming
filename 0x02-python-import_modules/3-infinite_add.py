292071cf5157eac97b9a40cfeeb3cc142ef77e77
aragakerubo <aragakerubo@gmail.com>
2024-01-06 19:54:30 -0500

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1
+    sum = 0
+    for i in range(1, argc + 1):
+        sum += int(argv[i])
+    print(sum)
