24a6c3de2b5bd3e3707ce2c259b49258e4cea014
aragakerubo <aragakerubo@gmail.com>
2024-01-11 19:18:24 -0500

    0. Squared simple

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
+
 def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
-    for row in new_matrix:
-        for column in row:
+    for index in range(len(matrix)):
+        new_matrix[index] = list(map(lambda x: x**2, matrix[index]))
 
+    return new_matrix
