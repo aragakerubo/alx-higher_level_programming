8835e1fbfa733791358864a2132565477f8706b0
aragakerubo <aragakerubo@gmail.com>
2024-01-12 19:05:31 -0500

    5. Number of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """Returns the number of keys in a dictionary
+
+    Args:
+        a_dictionary (dict): The dictionary to count the keys of.
+
+    Returns:
+        int: The number of keys in the dictionary.
+    """
+
+    return len(a_dictionary.keys())
