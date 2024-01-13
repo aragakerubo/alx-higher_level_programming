03c345488153dd72dcccc28220ebedc9a2eb9526
aragakerubo <aragakerubo@gmail.com>
2024-01-12 19:08:55 -0500

    6. Print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """Prints a dictionary by ordered keys
+
+    Args:
+        a_dictionary (dict): The dictionary to print.
+
+    Returns:
+        None
+    """
+    for key in sorted(a_dictionary.keys()):
+        print("{}: {}".format(key, a_dictionary[key]))
