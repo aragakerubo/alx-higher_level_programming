18687b5c8d47046d84509f7b2423fd6d61de5c58
aragakerubo <aragakerubo@gmail.com>
2024-01-22 17:03:15 -0500

    5. Raise exception

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def raise_exception():
+    """
+    Function that raises a type exception.
+
+    Returns:
+        Exception: The type exception.
+    """
+    raise TypeError
