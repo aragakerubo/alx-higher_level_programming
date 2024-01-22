3646c1a69490b545aeb8e08d589f23f1bdcf6003
aragakerubo <aragakerubo@gmail.com>
2024-01-22 17:04:42 -0500

    6. Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """
+    Function that raises a name exception with a message.
+
+    Args:
+        message (str): The message to be printed.
+
+    Returns:
+        Exception: The name exception.
+    """
+    raise NameError(message)
