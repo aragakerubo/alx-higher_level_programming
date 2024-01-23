9d2c3a2a2e5e73b0bd0db2fa5acaed52126768c1
aragakerubo <aragakerubo@gmail.com>
2024-01-22 19:09:31 -0500

    9. ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    """
+    Function that emulates the bytecode of a Python function.
+
+    Args:
+        a (int): The first value.
+        b (int): The second value.
+
+    Returns:
+        int: The result of the calculation.
+    """
+    result = 0
+    for item in range(1, 3):
+        try:
+            if item > a:
+                raise Exception("Too far")
+            else:
+                result += a**b / item
+        except:
+            result = b + a
+            break
+
+    return result
