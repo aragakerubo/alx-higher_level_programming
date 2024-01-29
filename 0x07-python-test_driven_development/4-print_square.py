e9eb38f752147355704c0472ec2e0999bb3044e8
aragakerubo <aragakerubo@gmail.com>
2024-01-29 16:39:27 -0500

    3. Print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module contains a function that prints a square with the character #.
+"""
+
+
+def print_square(size):
+    """
+    This function prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer.
+        TypeError: If size is a float and is less than 0.
+        TypeError: If size is less than 0.
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for x in range(size):
+        print("#" * size)
