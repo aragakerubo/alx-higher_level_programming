43a082030e49f88048094525ef0560686624b7ce
aragakerubo <aragakerubo@gmail.com>
2024-02-05 17:19:20 -0500

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,21 +1,3 @@
-class Base:
-    """My base class"""
-
-    __nb_instances = 0
-
-    def __init__(self):
-        Base.__nb_instances += 1
-        self.id = Base.__nb_instances
-
-
-class User(Base):
-    """My User class"""
-
-    pass
-
-
-u = User()
-print(u.id)
 #!/usr/bin/python3
 """Module for lookup method"""
 
