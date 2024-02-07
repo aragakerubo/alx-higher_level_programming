7ab12c2826cf8d7c46c4685451034476866b4bf3
aragakerubo <aragakerubo@gmail.com>
2024-02-06 19:55:19 -0500

    10. Student to JSON with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module for student"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        """Instantiation of a student
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns the dictionary description of the object
+
+        Args:
+            attrs (list): A list of attributes to return
+
+        Returns:
+            dict: The dictionary description of the object"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
