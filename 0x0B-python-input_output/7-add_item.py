0d433c46f9667e69fba7b8d02fadfe671cb391b4
aragakerubo <aragakerubo@gmail.com>
2024-02-06 19:30:38 -0500

    7. Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for add_item"""
+
+
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__("3-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__(
+        "4-load_from_json_file"
+    ).load_from_json_file
+    try:
+        items = load_from_json_file("add_item.json")
+    except:
+        items = []
+    items += sys.argv[1:]
+    save_to_json_file(items, "add_item.json")
