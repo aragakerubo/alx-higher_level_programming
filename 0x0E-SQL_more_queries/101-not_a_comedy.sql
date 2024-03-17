00b41eeaf63118342c7525ae810dcdc87753aa58
aragakerubo <aragakerubo@gmail.com>
2024-03-16 21:47:10 -0400

    18. No Comedy tonight!

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -8,12 +8,12 @@
 SELECT tv_shows.title
 FROM tv_shows
 WHERE id NOT IN (
-    SELECT show_id
-    FROM tv_show_genres
-    WHERE genre_id = (
-        SELECT id
-        FROM tv_genres
-        WHERE name = 'Comedy'
-        )
-    )
+    SELECT tv_shows.id
+    FROM tv_shows
+    JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres
+    ON tv_show_genres.genre_id = tv_genres.id
+    WHERE tv_genres.name = 'Comedy'
+)
 ORDER BY tv_shows.title ASC;
