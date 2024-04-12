61bd36f0deadc1df345cb52b311126c044f5e99e
aragakerubo <aragakerubo@gmail.com>
2024-04-12 00:54:19 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,11 +2,11 @@
 
 const args = process.argv;
 if (args.length <= 3) {
-    console.log(0);
+	console.log(0);
 } else {
-    const nums = args
-        .slice(2)
-        .map(Number)
-        .sort((a, b) => a - b);
-    console.log(nums[nums.length - 2]);
+	const nums = args
+		.slice(2)
+		.map(Number)
+		.sort((a, b) => a - b);
+	console.log(nums[nums.length - 2]);
 }
