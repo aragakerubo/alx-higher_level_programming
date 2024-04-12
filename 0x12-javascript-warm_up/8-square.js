68c00b54017e39628c7b97878412775dac972c45
aragakerubo <aragakerubo@gmail.com>
2024-04-12 00:41:46 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 
 const args = process.argv;
 if (isNaN(parseInt(args[2]))) {
-    console.log("Missing size");
+  console.log('Missing size');
 } else {
-    const size = parseInt(args[2]);
-    for (let i = 0; i < size; i++) {
-        console.log("X".repeat(size));
-    }
+  const size = parseInt(args[2]);
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
 }
