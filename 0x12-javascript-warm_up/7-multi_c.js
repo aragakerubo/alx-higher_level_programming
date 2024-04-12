374fdd02cce6305f9429c675869481ab37130e1a
aragakerubo <aragakerubo@gmail.com>
2024-04-12 00:40:02 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,9 +4,9 @@ const args = process.argv;
 const intArg = parseInt(args[2]);
 
 if (isNaN(intArg)) {
-    console.log("Missing number of occurrences");
+  console.log('Missing number of occurrences');
 } else {
-    for (let i = 0; i < intArg; i++) {
-        console.log("C is fun");
-    }
+  for (let i = 0; i < intArg; i++) {
+    console.log('C is fun');
+  }
 }
