96c6931ee6fe77e63fb3ef0aa65788069f1732ea
aragakerubo <aragakerubo@gmail.com>
2024-04-12 01:01:58 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 
-const args = process.argv;
-const num1 = parseInt(args[2]);
-const num2 = parseInt(args[3]);
-
 /**
  * add - returns the sum of two numbers
  * @param {number} a - first number
@@ -11,7 +7,7 @@ const num2 = parseInt(args[3]);
  * @returns {number} - sum of a and b
  */
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
 exports.add = add;
