86d11c87378bcad7358406726356cae21168c389
aragakerubo <aragakerubo@gmail.com>
2024-04-12 00:37:05 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const languages = ["C is fun", "Python is cool", "Javascript is amazing"];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const language of languages) {
-    console.log(language);
+  console.log(language);
 }
