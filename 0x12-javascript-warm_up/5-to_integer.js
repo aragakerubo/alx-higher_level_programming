0f64cc4265f9c4970c5084370f1433e7e7acc8f1
aragakerubo <aragakerubo@gmail.com>
2024-04-12 00:33:09 -0400

    0x12. JavaScript - Warm up

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 
 const args = process.argv;
 if (isNaN(parseInt(args[2]))) {
-    console.log("Not a number");
+    console.log('Not a number');
 } else {
-    console.log("My number: " + parseInt(args[2]));
+    console.log('My number: ' + parseInt(args[2]));
 }
