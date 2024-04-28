93519cbfa8323ca2399ea1896d8505786793aad0
aragakerubo <aragakerubo@gmail.com>
2024-04-27 21:30:17 -0400

    4. What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,14 +3,10 @@
 Fetches https://alx-intranet.hbtn.io/status
 """
 
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    with urllib.request.urlopen(
-        "https://alx-intranet.hbtn.io/status"
-    ) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+    response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(response.text)))
+    print("\t- content: {}".format(response.text))
