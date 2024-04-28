8f586cad02301757d3a853879b9eb13f769a0c41
aragakerubo <aragakerubo@gmail.com>
2024-04-27 21:04:47 -0400

    2. POST an email #0

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email,
+sends a POST request to the passed URL with the email as a parameter,
+and displays the body of the response (decoded in utf-8)
+"""
+
+import urllib.parse
+import urllib.request
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        data = urllib.parse.urlencode({"email": sys.argv[2]})
+        data = data.encode("ascii")
+        req = urllib.request.Request(sys.argv[1], data)
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode("utf-8"))
+    else:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
