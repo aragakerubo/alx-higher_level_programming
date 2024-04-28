32341b471f439b274739e0994e2d360e2e7fe5bf
aragakerubo <aragakerubo@gmail.com>
2024-04-27 22:04:49 -0400

    8. Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) == 1:
+        q = ""
+    else:
+        q = argv[1]
+    response = requests.post("http://0.0.0.0:5000/search_user", data={"q": q})
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(json_response["id"], json_response["name"]))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
